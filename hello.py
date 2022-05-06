52efd818654e837fa3ec92bda430bdfd6d5db605
Jw1Choi <wldnd12223@gmail.com>
2022-05-06 13:39:43 +0900

    3의배수

--- hello.py
+++ hello.py
@@ -1,1 +1,13 @@
+#과제 : 1부터 10까지 반복하는 과정에서 3의 배수일 경우, year를 출력하시오.
+#나머지 모든 경우는 숫자 그대로 출력
+
+for i in range(1,20):
+	if i % 15 ==0 :
+		print("year")
+	elif i %3 == 0:
+		print('dream')
+	elif i % 5 == 0:
+		print('yeardrem')
+	else: 
+		i = i
 print('hello')
